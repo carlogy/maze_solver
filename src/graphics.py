982747e5081ab06bf83255fa82a887c7de6483c4
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-05-14 09:12:13 -0400

    added a Point and Line class and a draw_line method to window class to draw lines on canvas. Instantiated a few lines on main.py to test new classes and methods.

--- src/graphics.py
+++ src/graphics.py
@@ -6,7 +6,7 @@ class Window:
         self.__root = Tk()
         self.__root.title("maze_zolver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__canvas = Canvas(self.__root bg="white", height=height, width=width)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__window_running = False
 
@@ -22,3 +22,26 @@ class Window:
 
     def close(self):
         self.__window_running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+                            self.point1.x,
+                            self.point1.y,
+                            self.point2.x,
+                            self.point2.y,
+                            fill=fill_color,
+                            width=2)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
-from graphics import Window
+from graphics import Line, Point, Window
 
 def main():
     win = Window(800, 600)
+    point1 = Point(100, 100)
+    point2 = Point(100, 200)
+    line1 = Line(point1, point2)
+    win.draw_line(line1, "black")
+    point3 = Point(400, 400)
+    point4 = Point(500, 400)
+    line2 = Line(point3, point4)
+    win.draw_line(line2, "green")
     win.wait_for_close()
 
 main()
