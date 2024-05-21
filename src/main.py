c3513eb3a0b5c8cd03b77186ef4412eb2a859bca
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-05-21 08:12:25 -0400

    updated cell class constructor to default to None and True for x,y coortidnates and if it has walls respectively. Also updated the draw method params to require x,y coordinates to draw itself.

--- src/main.py
+++ src/main.py
@@ -3,21 +3,30 @@ from cell import Cell
 
 def main():
     win = Window(800, 600)
-    point1 = Point(100, 100)
-    point2 = Point(100, 200)
-    line1 = Line(point1, point2)
-    win.draw_line(line1)
-    point3 = Point(400, 400)
-    point4 = Point(500, 400)
-    line2 = Line(point3, point4)
-    win.draw_line(line2, "green")
-    cell1 = Cell(True,True,True,True, 10, 20, 15, 25, win)
-    cell1.draw()
-    cell2 = Cell(True, False, True, False, 30, 40, 50, 60, win)
-    cell2.draw()
-    cell3 = Cell(False, False, True, True, 40, 50, 60, 70, win)
-    cell3.draw()
-    cell2.draw_move(cell3)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
+
     win.wait_for_close()
 
 main()
