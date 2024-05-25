76ed06760bb492e4522ebc6023be3dee4bec99de
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-05-25 08:36:49 -0400

    updated the list comprehension to instatiate the cells for each column, and completed the draw method's calculations for the position of each cell.

--- src/maze.py
+++ src/maze.py
@@ -1,3 +1,4 @@
+import time
 
 from cell import Cell
 
@@ -23,19 +24,20 @@ class Maze:
     def _create_cells(self):
         self.cells = []
         for i in range(self.num_cols):
-            column = [Cell(self.win) for i in range(self.num_rows // self.cell_size_y)]
+            column = [Cell(self.win) for i in range(self.num_rows)]
             self.cells.append(column)
         else:
             for i in range(len(self.cells)):
                 for j in range(len(self.cells[i])):
-                    self.cells[i][j]._draw_cell(i,j) #this was updated to call the draw_cell maze method on the specific cell in matrix.
+                    self._draw_cell(i,j)
 
-    def _draw_cell(self, I, J):
-        # This method should calculate the x/y position of the Cell based on i, j, the cell_size, and the x/y position of the Maze itself.
-        # The x/y position of the maze represents how many pixels from the top and left the maze should start from the side of the window.
-        cell_x = self.x1 * (I * self.cell_size_x)
-        cell_y = self.y1 * (J * self.cell_size_y)
-
+    def _draw_cell(self, i, j):
+        x1 = self.x1 + i * self.cell_size_x
+        y1 = self.y1 + j * self.cell_size_y
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+        self.cells[i][j].draw(x1,y1,x2,y2)
         self._animate()
     def _animate(self):
         self.win.redraw()
+        time.sleep(0.05)
